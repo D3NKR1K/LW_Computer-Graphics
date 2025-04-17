c411eccdc25851e0e8e0bc0681c106ed958092ed
Daniel Kuzmin <Den0Krik@yandex.ru>
2025-04-17 12:09:42 +0400

    FINAL

--- LAB4/main.py
+++ LAB4/main.py
@@ -4,12 +4,13 @@ from obj_parser import obj_parser
 from config_parser import config_parser
 from save_image import save_image
 from draw_image import draw_triangle
-from math import *
-from quaternion_operations import *
+
+from quaternion_operations import q_from_axis_angle, rotate_vector_quaternion
 from PIL import Image, ImageOps
 from typing import Dict
 
 IMAGE_SIZE = (1000, 1000)
+
 
 # Функция для построения матрицы поворота по Эйлеру
 def build_rotation_matrix(a: float, b: float, g: float) -> np.ndarray:
@@ -26,28 +27,33 @@ def build_rotation_matrix(a: float, b: float, g: float) -> np.ndarray:
 
 
 # Функция для вычисления нормалей вершин на основе данных модели
-def calculate_vertex_normals(
-  data: Dict[str, list], rotate_code
-) -> np.ndarray:
+def calculate_vertex_normals(data: Dict[str, list], rotate_code) -> np.ndarray:
     vn = np.zeros((len(data["v"]), 3), dtype=np.float32)
 
-  quaternions = np.tile(np.array([1, 0, 0, 0]), (len(data["v"]), 1))
+    quaternions = np.tile(np.array([1, 0, 0, 0]), (len(data["v"]), 1))
 
-  if rotate_code != 0:
-    q_rot = q_from_axis_angle(RENDER_CONFIG["axis_for_quater"], RENDER_CONFIG["rotation_angle_for_quater"])
-    quaternions = np.tile(q_rot, (len(data["v"]), 1))
+    if rotate_code != 0:
+        q_rot = q_from_axis_angle(
+            RENDER_CONFIG["axis_for_quater"], RENDER_CONFIG["rotation_angle_for_quater"]
+        )
+        quaternions = np.tile(q_rot, (len(data["v"]), 1))
 
-  for face in data["f"]:
-    v_indices = [vi[0] - 1 for vi in face]
-    vertices = np.array([data["v"][i] for i in v_indices])
+    for face in data["f"]:
+        v_indices = [vi[0] - 1 for vi in face]
+        vertices = np.array([data["v"][i] for i in v_indices])
 
-    if rotate_code == 0:
-      rotated_vertices = np.dot(vertices, rotation_matrix)
-    else:
-      rotated_vertices = np.array([
-        rotate_vector_quaternion(quaternions[i], v)
-        for i, v in zip(v_indices, vertices)
-      ]) + RENDER_CONFIG["translation_offset"]
+        if rotate_code == 0:
+            rotated_vertices = np.dot(vertices, rotation_matrix)
+        else:
+            rotated_vertices = (
+                np.array(
+                    [
+                        rotate_vector_quaternion(quaternions[i], v)
+                        for i, v in zip(v_indices, vertices)
+                    ]
+                )
+                + RENDER_CONFIG["translation_offset"]
+            )
 
         edge1 = rotated_vertices[1] - rotated_vertices[0]
         edge2 = rotated_vertices[2] - rotated_vertices[0]
@@ -56,8 +62,8 @@ def calculate_vertex_normals(
         for i in v_indices:
             vn[i] += face_normal
 
-  norms = np.linalg.norm(vn, axis=1)
-  return vn / norms[:, np.newaxis], quaternions
+    norms = np.linalg.norm(vn, axis=1)
+    return vn / norms[:, np.newaxis], quaternions
 
 
 # Функция для загрузки текстуры из файла
@@ -73,47 +79,54 @@ def load_texture(filepath):
 
 
 # Функция для построения модели и рендера изображения
-def build_model(data: Dict[str, list], matrix, z_buff, textures, rotate_code) -> np.ndarray:
-  # Вычисление нормалей вершин
-  vn, quaternions = calculate_vertex_normals(data, rotate_code)
-  # Scale
-  scale = RENDER_CONFIG["scale"]
+def build_model(
+    data: Dict[str, list], matrix, z_buff, textures, rotate_code
+) -> np.ndarray:
+    # Вычисление нормалей вершин
+    vn, quaternions = calculate_vertex_normals(data, rotate_code)
+    # Scale
+    scale = RENDER_CONFIG["scale"]
 
-  # Обработка каждой грани модели
-  for face in data["f"]:
-    # Индексы вершин грани
-    v_indices = [vi[0] - 1 for vi in face]
-    # Координаты вершин
-    vertices = np.array([data["v"][i] for i in v_indices])
+    # Обработка каждой грани модели
+    for face in data["f"]:
+        # Индексы вершин грани
+        v_indices = [vi[0] - 1 for vi in face]
+        # Координаты вершин
+        vertices = np.array([data["v"][i] for i in v_indices])
 
-    if rotate_code == 0:
-      # Поворот и смещение вершин
-      rotated_vertices = (
-              np.dot(vertices, rotation_matrix) + RENDER_CONFIG["translation_offset"]
-      )
-    else:
-      rotated_vertices = np.array([
-        rotate_vector_quaternion(quaternions[i], v)
-        for i, v in zip(v_indices, vertices)
-      ]) + RENDER_CONFIG["translation_offset"]
+        if rotate_code == 0:
+            # Поворот и смещение вершин
+            rotated_vertices = (
+                np.dot(vertices, rotation_matrix) + RENDER_CONFIG["translation_offset"]
+            )
+        else:
+            rotated_vertices = (
+                np.array(
+                    [
+                        rotate_vector_quaternion(quaternions[i], v)
+                        for i, v in zip(v_indices, vertices)
+                    ]
+                )
+                + RENDER_CONFIG["translation_offset"]
+            )
 
-    # Текстурные координаты
-    tex_coords = [data["vt"][vi[1] - 1] for vi in face]
+        # Текстурные координаты
+        tex_coords = [data["vt"][vi[1] - 1] for vi in face]
 
-    # Отрисовка треугольника
-    draw_triangle(
-      *rotated_vertices.reshape(-1),
-      vn[v_indices],
-      tex_coords,
-      textures,
-      matrix,
-      z_buff,
-      scale,
-      H,
-      W,
-    )
+        # Отрисовка треугольника
+        draw_triangle(
+            *rotated_vertices.reshape(-1),
+            vn[v_indices],
+            tex_coords,
+            textures,
+            matrix,
+            z_buff,
+            scale,
+            H,
+            W,
+        )
 
-  return matrix
+    return matrix
 
 
 if __name__ == "__main__":
@@ -124,46 +137,50 @@ if __name__ == "__main__":
     matrix = np.full((H, W, 3), (0, 0, 0), dtype=np.uint8)
     z_buff = np.full((H, W), np.inf)
 
-  print("F10 - обработка с поворотом по Эйлеру\n"
+    print(
+        "F10 - обработка с поворотом по Эйлеру\n"
         "F11 - обработка с поворотом по кватернионам\n"
-        "F12 - сохранение изображния\n")
+        "F12 - сохранение изображния\n"
+    )
 
-  while True:
-    if keyboard.is_pressed('F10'):
-      print("Обработка модели начата!")
+    while True:
+        if keyboard.is_pressed("F10"):
+            print("Обработка модели начата!")
             RENDER_CONFIG = config_parser("../data/config.txt")
 
             # Загрузка текстуры
             textures = load_texture(RENDER_CONFIG["texture_path"])
 
-      # Построение матрицы поворота
-      rotation_matrix = build_rotation_matrix(*RENDER_CONFIG["rotation_angles_for_eiler"])
+            # Построение матрицы поворота
+            rotation_matrix = build_rotation_matrix(
+                *RENDER_CONFIG["rotation_angles_for_eiler"]
+            )
 
             # Парсинг модели
             data = obj_parser(RENDER_CONFIG["input_path"])
 
-      image_matrix = build_model(data, matrix, z_buff, textures, 0)
+            image_matrix = build_model(data, matrix, z_buff, textures, 0)
 
             print("Изменения модели сохранены!")
 
-    if keyboard.is_pressed('F11'):
-      print("Обработка модели начата!")
+        if keyboard.is_pressed("F11"):
+            print("Обработка модели начата!")
 
-      RENDER_CONFIG = config_parser("../data/config.txt")
+            RENDER_CONFIG = config_parser("../data/config.txt")
 
-      # Загрузка текстуры
-      textures = load_texture(RENDER_CONFIG["texture_path"])
+            # Загрузка текстуры
+            textures = load_texture(RENDER_CONFIG["texture_path"])
 
-      # Парсинг модели
-      data = obj_parser(RENDER_CONFIG["input_path"])
+            # Парсинг модели
+            data = obj_parser(RENDER_CONFIG["input_path"])
 
-      # Построение модели
-      image_matrix = build_model(data, matrix, z_buff, textures, 1)
+            # Построение модели
+            image_matrix = build_model(data, matrix, z_buff, textures, 1)
 
-      print("Изменения модели сохранены!")
+            print("Изменения модели сохранены!")
 
-    if keyboard.is_pressed('F12'):
-      # Сохранение изображения
-      save_image(image_matrix, RENDER_CONFIG["output_path"])
-      print("Модель сохранена в виде изображения!")
-      break
+        if keyboard.is_pressed("F12"):
+            # Сохранение изображения
+            save_image(image_matrix, RENDER_CONFIG["output_path"])
+            print("Модель сохранена в виде изображения!")
+            break
